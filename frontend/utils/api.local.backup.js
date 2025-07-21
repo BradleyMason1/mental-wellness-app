38b871c0bdd89d0d6861e339e6393e2f2e73b9c6
Bradley Mason <bkmaso01@outlook.com>
2025-07-21 12:50:40 -0400

    fix

--- frontend/utils/api.local.backup.js
+++ frontend/utils/api.local.backup.js
@@ -3,4 +3,4 @@ import { Platform } from 'react-native';
 // Base URL for backend API
 // On Android emulators, `localhost` refers to the emulator itself.
 // 10.0.2.2 maps to the host machine running the server.
-export const API_BASE_URL = 'http://192.168.86.241:3000'; // your computer’s IP
+export const API_BASE_URL = 'http://Your-Computers-IPv4:3000'; // your computer’s IP
