8a2b1dbd74839bc986783f74ff405042ada5b6ec
Bradley Mason <bkmaso01@outlook.com>
2025-07-21 12:33:31 -0400

    Ignore api.js for local config

--- frontend/utils/api.js
+++
@@ -1,6 +0,0 @@
-import { Platform } from 'react-native';
-
-// Base URL for backend API
-// On Android emulators, `localhost` refers to the emulator itself.
-// 10.0.2.2 maps to the host machine running the server.
-export const API_BASE_URL = 'http://100.64.15.50:3000'; // your computer’s IP
